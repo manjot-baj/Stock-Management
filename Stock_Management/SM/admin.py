ba5ba54c4b9a0a8bca9fd8ecc1d659c6e3bf24ec
manjot-baj <manjot@kalpeshinfotech.com>
2020-02-05 11:46:16 +0530

    [added name of project in admin]

--- Stock_Management/SM/admin.py
+++ Stock_Management/SM/admin.py
@@ -3,6 +3,11 @@ from .enquiry import *
 from .dayBook import *
 from .invoice import *
 from .amc import *
+
+admin.site.site_header = 'Storeck'
+admin.site.site_title = 'Storeck'
+admin.site.index_title = 'Storeck Administration'
+
 admin.site.register(CostumerType)
 admin.site.register(EnquiryType)
 admin.site.register(EnquiryForm)
