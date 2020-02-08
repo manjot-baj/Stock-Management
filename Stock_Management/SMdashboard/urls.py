4b0281a3c30195fcc01190541e701c6c7bdf2fba
manjot-baj <manjot@kalpeshinfotech.com>
2020-02-08 13:21:57 +0530

    chg in template

---
+++ Stock_Management/SMdashboard/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+urlpatterns = [
+                path('', views.Dashboard.as_view(), name="dashboard"),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
