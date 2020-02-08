0544089b0aa5ca3e1ef03980e84ee4ca48c1fca6
manjot-baj <manjot@kalpeshinfotech.com>
2020-02-08 13:53:29 +0530

    [added base]

--- Stock_Management/SMdashboard/views.py
+++ Stock_Management/SMdashboard/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView, View
 
-# Create your views here.
+
+class Dashboard(View):
+    dashboard_template = 'SMdashboard/dashboard.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.dashboard_template)
+
+    def post(self, request, *args, **kwargs):
+        return render(request, self.dashboard_template)
