34d40e78f377acf72cfa15f7e17f45b5d1633694
manjot-baj <manjot@kalpeshinfotech.com>
2020-03-30 15:57:29 +0530

    new product model

--- Stock_Management/SM/invoice.py
+++ Stock_Management/SM/invoice.py
@@ -5,18 +5,7 @@ from .models import BaseModel
 from .company_data import CompanyDetail
 
 
-# from .PO import POData
-
-class ProductCategory(BaseModel):
-    name = models.CharField(max_length=50, null=True, blank=False)
-    description = models.TextField(max_length=50, null=True, blank=True)
-
-    def __str__(self):
-        return self.name
-
-
 class Product(BaseModel):
-    from . import utils
     TYPE_UOM = (
         ('Boxes', 'Boxes'),
         ('CFT', 'CFT'),
@@ -28,37 +17,26 @@ class Product(BaseModel):
         ('Killowgrams', 'Killowgrams'),
         ('Piece', 'Piece'),
     )
-    Product_Type = (
-        ('GST Tax', 'GST Tax'),
-        ('Bill of Supply', 'Bill of Supply'),
-        ('Standard', 'Standard'),
-        ('Tax', 'Tax'),
-    )
-
     Tax_Type = (
-        ('1% GST', '1'),
-        ('3% GST', '3'),
-        ('5% GST', '5'),
-        ('12% GST', '12'),
-        ('18% GST', '18'),
-        ('28% GST', '28'),
+        ('1', '1% GST'),
+        ('3', '3% GST'),
+        ('5', '5% GST'),
+        ('12', '12% GST'),
+        ('18', '18% GST'),
+        ('28', '28% GST'),
     )
-
-    number = models.CharField(default=utils.encode, max_length=50, null=True)
-    unit_price = models.FloatField(max_length=100, null=True, blank=False)
-    u_o_m = models.CharField(choices=TYPE_UOM, max_length=20, null=True, blank=False)
-    quantity = models.IntegerField(null=False, blank=False)
-    category = models.ForeignKey(ProductCategory, null=False, blank=False, on_delete=models.CASCADE)
-    product_type = models.CharField(choices=Product_Type, max_length=20, null=True, blank=False)
+    Product_Type = (('Product', 'Product'), ('Service', 'Service'))
+    type = models.CharField(max_length=50, choices=Product_Type, null=True, blank=False)
+    uom = models.CharField(choices=TYPE_UOM, max_length=20, null=True, blank=False)
+    sku = models.CharField(max_length=100, null=True, blank=True)
+    name = models.CharField(max_length=100, null=True, blank=False)
+    description = models.TextField(null=True, blank=True)
+    hsn = models.CharField(max_length=100, null=True, blank=True)
+    sac = models.CharField(max_length=100, null=True, blank=True)
+    unit_price = models.FloatField(null=True, blank=False, default=0.0)
     purchase_rate = models.FloatField(null=True, blank=True, default=0.0)
-    purchase_rate_currency = models.FloatField(null=True, blank=True, default=0.0)
-    h_s_n_or_s_a_c = models.CharField(max_length=100, null=True, blank=False)
-    s_k_u = models.CharField(max_length=100, null=True, blank=False)
-    tax = models.FloatField(null=True, blank=True, default=0.0)
-    c_e_s_s_percent = models.CharField(max_length=100, null=True, blank=False)
-    c_e_s_s = models.CharField(max_length=100, null=True, blank=False)
-    company = models.ForeignKey(CompanyDetail, on_delete=models.SET_NULL, null=True,
-                                blank=True)
+    tax = models.CharField(choices=Tax_Type, max_length=50, null=True, blank=False)
+    company = models.ForeignKey(CompanyDetail, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.name
