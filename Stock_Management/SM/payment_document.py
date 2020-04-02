449baa4e261aa69a868894022e4860d5773ab63c
manjot-baj <bajwa.mj78@gmail.com>
2020-04-02 18:46:00 +0530

    [added choices po and bill model and chg in model code]

--- Stock_Management/SM/payment_document.py
+++ Stock_Management/SM/payment_document.py
@@ -8,11 +8,11 @@ from .choices import Places, PaymentStatus, Prices, TaxType, TypeUOM, Payment_st
 
 class PaymentDocument(BaseModel):
     client = models.ForeignKey(Client, on_delete=models.CASCADE, null=True, blank=True)
-    payment_status = models.CharField(null=True, blank=True, choices=Payment_status)
+    payment_status = models.CharField(max_length=50, null=True, blank=True, choices=Payment_status)
     number = models.CharField(null=True, blank=True, max_length=100)
     reference = models.CharField(null=True, blank=True, max_length=100)
     amount = models.DecimalField(decimal_places=2, max_digits=10, default=0.00)
-    payment_type = models.CharField(null=True, blank=True, choices=Payment_Type)
+    payment_type = models.CharField(max_length=50, null=True, blank=True, choices=Payment_Type)
     pay_date = models.DateTimeField(default=timezone.now, null=True, blank=False)
     bank_charges = models.DecimalField(decimal_places=2, max_digits=10, default=0.00)
     note = models.TextField(null=True, blank=True)
