dd1136f2a3d1c8449d9c12c99f1d34f021143b3b
manjot-baj <manjot@kalpeshinfotech.com>
2020-04-10 20:21:30 +0800

    chg in quotation

--- Stock_Management/SM/quotation.py
+++ Stock_Management/SM/quotation.py
@@ -4,9 +4,14 @@ from .models import BaseModel
 from .company_data import CompanyDetail, Client
 from .product import Product
 from .choices import Places, PaymentStatus, Prices, TaxType, TypeUOM
+import random
+
+def random_string():
+    return str(random.randint(10000, 99999))
 
 
 class Quotation(BaseModel):
+    no = models.CharField(default=random_string, max_length=50, null=True)
     number = models.CharField(max_length=100, null=True, blank=True)
     client = models.ForeignKey(Client, on_delete=models.CASCADE, null=True, blank=False)
     ship_to = models.TextField(null=True, blank=False)
@@ -14,9 +19,10 @@ class Quotation(BaseModel):
     due_date = models.DateField(null=True, blank=False)
     grand_total = models.FloatField(null=True, blank=True, default=0.0)
     company = models.ForeignKey(CompanyDetail, on_delete=models.SET_NULL, null=True, blank=False)
+    with_gst = models.BooleanField(null=True, blank=True)
 
     def __str__(self):
-        return self.number
+        return self.no
 
     class Meta:
         db_table = 'quotation'
@@ -34,3 +40,25 @@ class Quotation_lines(BaseModel):
     class Meta:
         db_table = 'quotation_line'
         verbose_name_plural = 'Quotation Line'
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        if self.pk is None:
+            if self.tax == '0':
+                company = list(Quotation.objects.filter(no=self.quotation).values('company'))
+                count = Quotation.objects.filter(company_id=company[0].get('company'), with_gst=False).count()
+                Quotation.objects.filter(no=self.quotation).update(number=count + 1, with_gst=False)
+                quotation_save = super(Quotation_lines, self).save(force_insert=False, force_update=False, using=None,
+                                                              update_fields=None)
+
+
+            elif not self.tax == '0':
+                company = list(Quotation.objects.filter(no=self.quotation).values('company'))
+                count = Quotation.objects.filter(company_id=company[0].get('company'), with_gst=True).count()
+                Quotation.objects.filter(no=self.quotation).update(number=count + 1, with_gst=True)
+                quotation_save = super(Quotation_lines, self).save(force_insert=False, force_update=False, using=None,
+                                                                   update_fields=None)
+
+        else:
+            quotation_save = super(Quotation_lines, self).save(force_insert=False, force_update=False, using=None,
+                                                          update_fields=None)
+        return quotation_save
